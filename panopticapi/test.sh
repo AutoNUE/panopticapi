c19a31faf08cb2f7cafab19827a89a7b7ac2cfdb
Girish Varma <>
2019-10-14 14:55:44 +0530

    changes

--- panopticapi/test.sh
+++ panopticapi/test.sh
@@ -1,4 +1,4 @@
-python3 evaluation.py --gt_json_file /home/chrizandr/idd20kII/gtFine/val_panoptic.json \
-                     --pred_json_file /home/chrizandr/idd20kII/gtFine/val_panoptic.json \
-                     --gt_folder /home/chrizandr/idd20kII/gtFine/val_panoptic \
-                     --pred_folder /home/chrizandr/idd20kII/gtFine/val_panoptic
+python3 evaluation.py --gt_json_file /home/girish/idd/datasets/IDD/idd_full_test/gtFine/test_panoptic.json \
+                     --pred_json_file /home/girish/idd/datasets/IDD/idd_full_test/gtFine/test_panoptic.json \
+                     --gt_folder /home/girish/idd/datasets/IDD/idd_full_test/gtFine/test_panoptic \
+                     --pred_folder /home/girish/idd/datasets/IDD/idd_full_test/gtFine/test_panoptic
